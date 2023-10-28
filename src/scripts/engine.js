3650e29ab2d1a273dab08afa48d5ddd336fae5fd
Felipe Maia <felpsnaitor@gmail.com>
2023-10-27 23:11:50 -0300

    finalizando a base do main.css e começãndo o JS

---
+++ src/scripts/engine.js
@@ -0,0 +1,16 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        socoreBox: document.getElementById("score-points"),
+    },
+    card
+
+}
+
+
+function init(){
+
+}
+
+init()
