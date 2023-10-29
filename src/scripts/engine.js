d585d957673981efa1e5538715fb867f210ebd75
Felipe Maia <felpsnaitor@gmail.com>
2023-10-29 16:48:52 -0300

    feito o stades e cardData

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,10 +4,50 @@ const state = {
         computerScore: 0,
         socoreBox: document.getElementById("score-points"),
     },
-    card
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards:{
+    player: document.getElementById("player-field-card"),
+    computer: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
 
 }
+cont caminhoDaImagem = ".src/assets/icons/"
+const cardData = [
+    {
+        id: 0,
+        name: "Dragão Branco dos Olhos Azuis",
+        type: "Papel",
+        img: `${caminhoDaImagem}+dragon.png`,
+        winOf: [1],
+        loseOf: [2],
 
+    },
+    {
+        id: 1,
+        name: "Mago Negro",
+        type: "Pedra",
+        img: `${caminhoDaImagem}+magician.png`,
+        winOf: [2],
+        loseOf: [0],
+
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Tesoura",
+        img: `${caminhoDaImagem}+exodia.png`,
+        winOf: [0],
+        loseOf: [1],
+
+    },
+]
 
 function init(){
 
