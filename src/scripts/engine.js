375c8ec42819ac4854344555f726931f0d3f03f6
Felipe Maia <felpsnaitor@gmail.com>
2023-11-03 22:05:21 -0300

    colocando audio de fundo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -196,6 +196,8 @@ function init(){
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer)
 
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 }
 
 init()
